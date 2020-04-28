e864dca8ae477ffcaf58eee04e85b22abbe61bd7
NoahEFaust <noah@fausthouse.org>
2020-04-28 17:32:51 -0400

    Profamity watcher (#2)
    * Delete .env
    
    * Added greeting DM for joining server
    
    * Add command to return who is a good boy
    
    * Add blackjack and hookers filler, some error handling to roll function.
    
    * Add a feature that keeps and eye out for bad words
    
    Co-authored-by: alexanderWhile <60705459+alexanderWhile@users.noreply.github.com>
    Co-authored-by: unknown <alexanderwhile@gmail.com>
    Co-authored-by: Noah Faust <Noah.Faust@lakeshore.com>

--- boy.py
+++ boy.py
@@ -10,7 +10,10 @@ from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+
+client = discord.Client()
 bot = commands.Bot(command_prefix='<:IshBot:704803379452313710> ')
+
 
 
 @bot.event
@@ -26,30 +29,31 @@ async def on_member_join(member):
     )
 
 
-@bot.command(name = 'roll', help='Simulates rolling dice.')
+@bot.command(name='roll', help='Simulates rolling dice.')
 async def roll(ctx, number_of_dice, number_of_sides):
     try:
         number_of_dice = int(float(number_of_dice))
         number_of_sides = int(float(number_of_sides))
         dice = [
-            str(random.choice(range(1, number_of_sides +1)))
+            str(random.choice(range(1, number_of_sides + 1)))
             for _ in range(number_of_dice)
             ]
         await ctx.send(', '.join(dice))
     except:
         await ctx.send("I don't understand what you want me to do.")
 
-@bot.command(name = 'blackjack', help='Play a game of Blackjack with Ishbot!')
+
+@bot.command(name='blackjack', help='Play a game of Blackjack with Ishbot!')
 async def deal(ctx):
     await ctx.send("I don't know how to play that yet, but I will soon!")
 
-@bot.command(name = 'hookers', help="Wouldn't you like to know?")
+
+@bot.command(name='hookers', help="Wouldn't you like to know?")
 async def hooker(ctx):
     await ctx.send("We don't have hookers just yet, sorry")
 
 
-
-@bot.command(name = 'who_is_a_good_boy', help='Returns who is a good boy.')
+@bot.command(name='who_is_a_good_boy', help='Returns who is a good boy.')
 async def who_is_a_good_boi(ctx):
     if ctx.author.nick != None:
         await ctx.send(f'*barks happily at {ctx.author.nick}*')
@@ -61,5 +65,19 @@ async def who_is_a_good_boi(ctx):
 async def echo(ctx, message:str):
     print(message)
 
+
+
+@client.event
+async def on_message(message):
+    profamity_list = ['heck', 'darn', 'poop', 'frick', 'stupidhead']
+    there_are_bad_words = False
+
+    for bad_word in profamity_list:
+        if bad_word in message.content:
+            there_are_bad_words = True
+            break
+
+    if there_are_bad_words:
+        await message.channel.send('Watch your profamity!')
 
 bot.run(TOKEN)
