5549859094e56f51fee5b8365f3a72b4cf094f8c
Estelle Poulin <dev@inspiredby.es>
2020-04-28 18:54:54 -0400

    Revert "Fixed promaity filter"
    This reverts commit f4af8125242ddd7c7db1db27c8f195d8da9daf6a.

--- boy.py
+++ boy.py
@@ -65,7 +65,7 @@ async def echo(ctx, message:str):
 
 
 
-@bot.event
+@client.event
 async def on_message(message):
     profamity_list = ['heck', 'darn', 'poop', 'frick', 'stupidhead']
     there_are_bad_words = False
